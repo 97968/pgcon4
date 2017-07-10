2c41b65fd5e694e4532fd1ad43a0c990764a5c54
橋本 尚儒 <97968@bsp.co.jp>
2017-07-10 17:15:49 +0900

    fix variable name

--- Q2.java
+++ Q2.java
@@ -1,4 +1,3 @@
-スコード
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
@@ -7,36 +6,32 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] list = new int[26 * 26];
-        Arrays.fill(list, 0);
-        int a = br.read();
-        int b;
-        while((b = br.read()) != -1){
-            if(a != '\n' && b!= '\n'){
-                list[(a - 'A') * 26 + (b - 'A')]++;
+        int ch1 = '\n';
+        int ch2;
+        while((ch2 = br.read()) != -1){
+            if(ch1 != '\n' &&'A' <= ch2 && ch2 <= 'Z'){
+                list[(ch1 - 'A') * 26 + (ch2 - 'A')]++;
             }
-            a = b;
+            ch1 = ch2;
         }
-        boolean f = true;
-        for(int i = 0; i < 10; i++){
-            int max  = 0, value = 0;
+        int i;
+        for(i = 0; i < 10; i++){
+            int max  = 0, idx = 0;
             for(int j = 0; j < 26 * 26; j++){
                 if(max < list[j]){
-                    f = false;
                     max = list[j];
-                    value = j;
+                    idx = j;
                 }
             }
             if(max == 0){
                 break;
             }
-            char c = (char)('A' + value / 26);
-            char d = (char)('A' + value % 26);
-            String s = c + "" + d;
-            System.out.println(c + "" + d + " " + max);
-            list[value]= 0;
-            max = 0;
+            char ch3 = (char)('A' + idx / 26);
+            char ch4 = (char)('A' + idx % 26);
+            System.out.println(ch3 + "" + ch4 + " " + max);
+            list[idx]= 0;
         }
-        if(f){
+        if(i == 0){
             System.out.println();
         }
     }
